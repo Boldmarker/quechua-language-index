ee3d013e81018aaf33f108935169436d1e71afbd
Tyler Lee <tylerkinglee@gmail.com>
2023-08-27 23:07:31 +0000

    updates

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-ENDPOINT
+AIRTABLE_BASE_ID = 'appusI3ZyTQerSfSa'
+
+AIRTABLE_TABLE_NAME = 'Words'
+
+ENDPOINT = 'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
+
+#Python Requests Headers
+headers = {
+    "Authorization": "Bearer YOUR_API_KEY",
+    "Content-Type": "application.json"
+}
+
