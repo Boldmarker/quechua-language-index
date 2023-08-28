f39dc45fedb5b4798292c946cf4bf76cc381ef6c
Tyler Lee <tylerkinglee@gmail.com>
2023-08-28 03:44:10 +0000

    production code (v1)

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
 import requests
 
-AIRTABLE_BASE_ID = 'appusI3ZyTQerSfSa'
 AIRTABLE_API_KEY = 'patFfF4vN9DZI2qMc.ef219b1cb4cc4b365f432c23f0738551ceae78cdfced6cace4517167b5668876'
-AIRTABLE_TABLE_NAME = 'Words'
+AIRTABLE_BASE_ID = 'appusI3ZyTQerSfSa'
+AIRTABLE_TABLE_ID = 'tbleEou1VywgQ0Kad'
 
-endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
+endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_ID}'
 
 # Python Requests Headers
 headers = {
     "Authorization": f"Bearer {AIRTABLE_API_KEY}",
-    "Content-Type": "application.json"
+    "Content-Type": "application/json"
 }
 
 # Data / Content
 data = {
-  "records": [
-    {
-      "fields": {
-        "Word": "Choose",
-        "Root": "Choose",
-        "Past Tense": "Chose",
-        "Palabra": "Escoger",
-        "Raiz": "Escog",
-        "Shimi": "Akllay",
-        "Sapi": "Aklla",
-        "Word Type": [
-          "Verb"
-        ],
-        "Category": [
-          "Action"
-        ],
-      }
-    }
-    }
+"fields": {
+    "Word": "Choose",
+    "Root": "Choose",
+    "Past Tense": "Chose",
+    "Palabra": "Escoger",
+    "Raiz": "Escog",
+    "Shimi": "Akllay",
+    "Sapi": "Aklla",
+    "Word Type": ["Verb"],
+    "Category": ["Action"],
+  }
+}
 
 # HTTP Methods
 # What is the method for "create" -> POST
